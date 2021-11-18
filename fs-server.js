04059a5cbbb9783f330c644dee5911e2355df7fe
Софья Нильга <nlgs1337@gmail.com>
2021-11-18 17:59:34 +0200

    node modules

--- fs-server.js
+++ fs-server.js
@@ -6,3 +6,18 @@ fs.mkdir(path.join(__dirname, 'notes',), err =>{
     if (err) throw err
     console.log('created')
 })
+//Додавання данних в текст
+fs.appendFile('testFile.txt', ' This line is beyond the end.', (err) => {
+    if(err) throw err;
+    console.log('Data has been added!');
+});
+//Перейменування файлу
+fs.rename('testFile.txt', 'newTestFile.txt', (err) => {
+    if(err) throw err;
+    console.log('File renamed successfully!');
+});
+//Видалення файлу
+fs.unlink('newTestFile.txt', (err) => {
+    if(err) throw err;
+    console.log('File deleted successfully!');
+});
--- os-server.js
+++ os-server.js
@@ -1,2 +1,12 @@
 const os = require('os');
 
+//повертає ім'я хоста ОС
+console.log(os.hostname())
+//повертає архітектуру ЦП ОС
+console.log(os.arch())
+//масив об'єктів, інофрмація про кожне ядро
+console.log(os.cpus())
+//об'єм вільної системної пам'яті в байтах
+console.log(os.freemem())
+//об'єм загальний системної пам'яті в байтах
+console.log(os.totalmem())
